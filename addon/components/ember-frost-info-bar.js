0eb7afd2573fcd6981324b63d84a17ba69adce46
Roxanne Panthaky <rpanthak@ciena.com>
2016-02-21 13:02:39 -0500

    component and css changes for open source

--- addon/components/ember-frost-info-bar.js
+++ addon/components/ember-frost-info-bar.js
@@ -1,7 +1,5 @@
 import Ember from 'ember'
-import layout from './template'
 
 export default Ember.Component.extend({
-  layout: layout,
   classNames: ['ember-frost-info-bar']
 })
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/ember-frost-info-bar'
   }
 
   return ENV
