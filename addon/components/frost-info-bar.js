ed67a806be5dc50ece1eb10527b1711d5ddea429
Quincy <qle@ciena.com>
2016-05-17 15:09:16 -0400

    refactor info bar using block slots

--- addon/components/frost-info-bar.js
+++ addon/components/frost-info-bar.js
@@ -1,5 +1,7 @@
 import Ember from 'ember'
+import layout from '../templates/components/frost-info-bar'
 
 export default Ember.Component.extend({
-  classNames: ['frost-info-bar']
+  classNames: ['frost-info-bar'],
+  layout: layout
 })
