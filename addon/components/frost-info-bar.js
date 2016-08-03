2c161b7fba03497f547289884da97491403bd295
Olufemi Adeojo <oadeojo@ciena.com>
2016-08-03 12:11:55 -0400

    updated to used blocl-slots >1.0.0

--- addon/components/frost-info-bar.js
+++ addon/components/frost-info-bar.js
@@ -1,7 +1,8 @@
 import Ember from 'ember'
 import layout from '../templates/components/frost-info-bar'
+import BlockSlotMixin from 'ember-block-slots'
 
-export default Ember.Component.extend({
+export default Ember.Component.extend( BlockSlotMixin, {
   classNames: ['frost-info-bar'],
   layout: layout
 })
