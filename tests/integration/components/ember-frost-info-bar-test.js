e3f1b26438c0173ff6cc5aba3b4ac9b75bad52d2
Michael Carroll <juwara0@users.noreply.github.com>
2018-01-16 11:11:26 -0600

    update path to ember-test-utils setup-component-test helper

--- tests/integration/components/ember-frost-info-bar-test.js
+++ tests/integration/components/ember-frost-info-bar-test.js
@@ -1,10 +1,9 @@
 import {expect} from 'chai'
 import {$hook, initialize} from 'ember-hook'
+import {integration} from 'ember-test-utils/test-support/setup-component-test'
 import hbs from 'htmlbars-inline-precompile'
 import {beforeEach, describe, it} from 'mocha'
 import {registerMockComponent, unregisterMockComponent} from '../../helpers/mock-component'
-
-import {integration} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
 const test = integration('ember-frost-info-bar')
 describe(test.label, function () {
